7eaa82dafa12b36e1be3c927b93e67293c4b2b51
Mosen <mosen1@gmail.com>
2014-02-18 17:52:49 +1100

    Add RSpec example for /configurationprofiles/get/all

--- spec/api/configurationprofiles_spec.rb
+++ spec/api/configurationprofiles_spec.rb
@@ -1,11 +1,36 @@
 require 'spec_helper'
 require 'shared_examples_http'
-require 'cfpropertylist'
+require 'shared_contexts'
 
-describe '/configurationprofiles' do
+describe '/configurationprofiles', fakefs: true do
+
+  # TODO: stub enrollment and trust profiles
 
   describe '/get/all' do
+    before do
+      get '/configurationprofiles/get/all', { 'id' => 'W1111GTM4QQ' }
+    end
 
+    it_behaves_like 'an xml plist response'
+
+    context 'with parsed plist response' do
+      include_context 'with parsed plist response'
+
+      it 'contains a configuration array' do
+        expect(plist_hash).to have_key('configuration')
+        expect(plist_hash['configuration']).to be_instance_of(Array)
+      end
+
+      it 'contains an enrollment array' do
+        expect(plist_hash).to have_key('enrollment')
+        expect(plist_hash['enrollment']).to be_instance_of(Array)
+      end
+
+      it 'contains a trust array' do
+        expect(plist_hash).to have_key('trust')
+        expect(plist_hash['trust']).to be_instance_of(Array)
+      end
+    end
   end
 
 end
